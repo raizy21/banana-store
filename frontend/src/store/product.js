addfba9eda9ebb15e56864dd48b009cd3228e9b6
Razvan Andrei <chiper_raizy@yahoo.com>
2025-04-27 14:50:03 +0200

    [CODE] add the delete function to the webpage

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -30,4 +30,24 @@ export const useProductStore = create((set) => ({
     const data = await res.json();
     set({ products: data.data });
   },
+
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) {
+      return { success: false, message: data.message };
+    }
+
+    // update the ui immediately, without needing to refetch the data
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid),
+    }));
+
+    return {
+      success: true,
+      message: "product deleted successfully",
+    };
+  },
 }));
