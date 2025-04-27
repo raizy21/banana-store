fff1d1307f498d557cb590ecf5f3725d069e6d13
Razvan Andrei <chiper_raizy@yahoo.com>
2025-04-27 08:06:44 +0200

    [CODE] adding the homr page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,41 @@
+import { Container, VStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <Container maxW="container.xl" p={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, black,yellow.400)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          current products
+        </Text>
+
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight={"bold"}
+          color={"yellow.400"}
+        >
+          no products found
+        </Text>
+        <Link to={"/create"}>
+          <Text
+            as="span"
+            color="black"
+            fontSize="xl"
+            _hover={{ textDecoration: "underline" }}
+          >
+            create a product
+          </Text>
+        </Link>
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
