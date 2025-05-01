3c21182b9fdea23810bc720526b0f91f7bb748fa
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-01 10:30:59 +0200

    [CODE] fixing the main layout with the flying footer

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,21 +1,20 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Box, useColorModeValue } from "@chakra-ui/react";
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = () => {
+  const bgColor = useColorModeValue("rgb(121, 111, 16)", "rgb(241, 233, 138)");
   return (
-    <Box
-      minH={"100vh"}
-      bg={useColorModeValue("rgb(121, 111, 16)", "rgb(241, 233, 138)")}
-    >
+    <Flex direction="column" minH="100vh" bg={bgColor} overflow="hidden">
       <Navbar />
-      <main className=" py-4">
+      {/* main content should expand to push footer down */}
+      <Box as="main" flex="1" py={4} bg={bgColor} overflow="auto">
         <Outlet />
-      </main>
+      </Box>
       <Footer />
-    </Box>
+    </Flex>
   );
 };
 
