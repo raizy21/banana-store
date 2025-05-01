e91cd0ff110af2ba65ed19180807a56eca8d5141
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-01 09:50:11 +0200

    [CODE] adding the down part of footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,29 +1,15 @@
 import UpperPartFooter from "./UpperPartFooter";
-
-// import TextDownPart from "./footer/TextDownPart";
-// import Linkedin from "./footer/Linkedin";
-// import Instagram from "./footer/Instagram";
-// import Github from "./footer/Github";
-// import Youtube from "./footer/Youtube";
-// import United from "./footer/United";
+import DownPartFooter from "./DownPartFooter";
 
 const Footer = () => {
   return (
-    <>
-      <footer>
-        {/* the upper part */}
-        <UpperPartFooter />
+    <footer>
+      {/* the upper part */}
+      <UpperPartFooter />
 
-        {/* the down part */}
-        {/* <TextDownPart /> */}
-
-        {/* <Linkedin /> */}
-        {/* <Instagram /> */}
-        {/* <Github /> */}
-        {/* <Youtube /> */}
-        {/* <United /> */}
-      </footer>
-    </>
+      {/* the down part */}
+      <DownPartFooter />
+    </footer>
   );
 };
 
