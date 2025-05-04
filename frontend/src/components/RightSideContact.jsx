53792a61e6d11b01af812848d45cce4502988a33
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-04 08:40:47 +0200

    [CODE] adding the right side to the contact page

--- frontend/src/components/RightSideContact.jsx
+++ frontend/src/components/RightSideContact.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { imgBanannaArray } from "../data/imageEventsBanana.js";
+import { Box, Image } from "@chakra-ui/react";
+
 const RightSideContact = () => {
-  return <div>right side contact</div>;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex(
+        (prevIndex) => (prevIndex + 1) % imgBanannaArray.length
+      );
+    }, 4000); // change image every 4 seconds
+
+    return () => clearInterval(interval); // clean up the interval when component unmounts
+  }, []);
+  return (
+    <Box display={{ base: "none", lg: "flex" }} borderColor="accent">
+      <Image
+        id="imageContact"
+        src={imgBanannaArray[currentImageIndex].images}
+        alt="event"
+        borderRadius="full"
+        h="35rem"
+        w="35rem"
+      />
+    </Box>
+  );
 };
 
 export default RightSideContact;
---
+++ frontend/src/data/imageEventsBanana.js
@@ -0,0 +1,20 @@
+import bananaEvents from "../assets/banana-events.jpg";
+import bananaEvents1 from "../assets/banana-events1.jpg";
+import bananaEvents2 from "../assets/banana-events2.jpg";
+import bananaEvents3 from "../assets/banana-events3.jpg";
+
+// banana data
+export const imgBanannaArray = [
+  {
+    images: bananaEvents,
+  },
+  {
+    images: bananaEvents1,
+  },
+  {
+    images: bananaEvents2,
+  },
+  {
+    images: bananaEvents3,
+  },
+];
